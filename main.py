134189293a42850bf64b3a7f2a982c0c5e87f933
Justin Schots <justinschots13@gmail.com>
2024-04-25 19:02:22 +0200

    Added function for CL and requirements.txt functions

--- main.py
+++ main.py
@@ -2,10 +2,23 @@
 # It is used to split the data into training and validation sets and to further train the model.
 
 import os
-from logic.data_handler.trainValSplit import split_data
+
+
+def install_requirements(requirements_file):
+    with open(requirements_file, 'r') as file:
+        requirements = file.readlines()
+        for req in requirements:
+            req = req.strip()  # Remove leading/trailing whitespace
+            if req.startswith('#'):
+                continue  # Skip comments
+            os.system(f'pip install {req}')
 
 
 def main():
+
+    from logic.data_handler.trainValSplit import split_data
+    from logic.cl_yolo import continue_training
+
     # Define the root path of the input directory for YOLO data
     yolo_input_root = os.path.join(os.getcwd(), 'yolo_input')
 
@@ -15,6 +28,20 @@ def main():
     # Split the data into training and validation sets directly from the input
     split_data(yolo_input_root, train_val_root, 0.7)
 
+    # Set the path to the weights file
+    weights_path = 'models/best.pt'
+
+    # Set the path to the data configuration file
+    data_yaml = 'datasets/data-chimp.yaml'
+
+    # You can adjust these parameters as needed
+    img_size = 640
+    batch_size = 16
+    epochs = 50
+
+    continue_training(weights_path, data_yaml, img_size, batch_size, epochs)
+
 
 if __name__ == "__main__":
+    install_requirements('requirements.txt')
     main()
