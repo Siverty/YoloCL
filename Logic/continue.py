67298ab520220c6cb2ebf74a5cc65e3bd4799d8d
Justin_Laptop <justinschots13@gmail.com>
2024-04-25 10:50:58 +0200

    concept code

--- Logic/continue.py
+++ Logic/continue.py
@@ -1,3 +1,5 @@
+# Description: This script is used to continue training from a pretrained model.
+
 import ultralytics
 
-python train.py --weights path/to/best.pt  # start from pretrained model
+# python train.py --weights path/to/best.pt  # start from pretrained model
