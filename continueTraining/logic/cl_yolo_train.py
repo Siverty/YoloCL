ce01d2d466aa9d13947f832b973b8bc9500a30e7
Justin Schots <justinschots13@gmail.com>
2024-05-06 19:45:01 +0200

    added mlflow handling

--- continueTraining/logic/cl_yolo_train.py
+++ continueTraining/logic/cl_yolo_train.py
@@ -6,7 +6,8 @@ import mlflow.pytorch
 mlflow.set_tracking_uri('http://localhost:5000')
 
 
-def continue_training(weights, model_specifics, image_size, batch_size, epochs, checkpoint, current_experiment):
+def continue_training(weights: str, model_specifics: str, image_size: int, batch_size: int, epochs: int,
+                      checkpoint: int, current_experiment: str):
     from ultralytics import YOLO
     import torch
     from datetime import datetime
@@ -18,8 +19,18 @@ def continue_training(weights, model_specifics, image_size, batch_size, epochs, 
     else:
         experiment_id = mlflow.create_experiment(current_experiment)
 
-    # Start a mlflow run
-    with mlflow.start_run(experiment_id=experiment_id):
+    # Remove the '_cl' extension from the experiment name
+    new_name = current_experiment[:-3]
+
+    now_time = datetime.now().strftime("%Y-%m-%d %H:%M")
+
+    # Get the number of existing runs within the experiment
+    existing_runs = mlflow.search_runs(experiment_ids=[experiment_id])
+    num_runs = len(existing_runs)
+    name = f"{new_name}_{num_runs + 1}"
+
+    # Start a mlflow run with a custom run name
+    with mlflow.start_run(experiment_id=experiment_id, run_name=name):
         # Log parameters
         mlflow.log_params({
             "weights": weights,
@@ -27,7 +38,9 @@ def continue_training(weights, model_specifics, image_size, batch_size, epochs, 
             "image_size": image_size,
             "batch_size": batch_size,
             "epochs": epochs,
-            "checkpoint": checkpoint
+            "checkpoint": checkpoint,
+            "experiment": current_experiment,
+            "date": now_time
         })
 
         # Load the existing model
@@ -39,24 +52,23 @@ def continue_training(weights, model_specifics, image_size, batch_size, epochs, 
             print('!!!ATTENTION: GPU not available. Training on CPU!!!')
 
         # Train the model
-        for epoch in range(epochs):
+        for epoch in range(1):
             # Insert training code here, assume some metrics are calculated
             model.train(device=gpu_preference, data=model_specifics,  imgsz=image_size, batch=batch_size, epochs=epochs,
-                        save_period=checkpoint)
-            # Example: log loss and accuracy for each epoch (adjust according to actual available metrics)
+                        save_period=checkpoint, save_dir=0)
+            # log loss and accuracy for each epoch (adjust according to actual available metrics)
             mlflow.log_metrics({"loss": 0.01 * epoch, "accuracy": 0.98 - 0.01 * epoch}, step=epoch)
 
-        # Save the trained model
-        root = os.getcwd()
-        save_dir = os.path.join(root, 'models', 'continue_training')
-        model_count = len(os.listdir(save_dir))
-        new_model_name = datetime.now().strftime("%Y-%m-%d_%H-%M")
-        new_model_name = f"{model_count + 1}_{new_model_name}"
+            # Save the trained model
+            root = os.getcwd()
+            save_dir = os.path.join(root, 'data', new_name, 'models', 'continue_training')
+            model_count = len(os.listdir(save_dir))
+            new_model_name = f"{model_count + 1}"
 
-        # Save the model as 'best_x.pt' in /models/continue_training
-        model_path = f"{save_dir}/best_{new_model_name}.pt"
-        model.save(model_path)
-        print(f"Model saved as best_{new_model_name}.pt in {save_dir}")
+            # Save the model as 'best_x.pt' in /models/continue_training
+            model_path = f"{save_dir}/best_{new_model_name}.pt"
+            model.save(model_path)
+            print(f"Model saved as best_{new_model_name}.pt in {save_dir}")
 
-        # Log model as an artifact
-        mlflow.log_artifact(model_path)
+            # Log model as an artifact
+            mlflow.log_artifact(model_path)
